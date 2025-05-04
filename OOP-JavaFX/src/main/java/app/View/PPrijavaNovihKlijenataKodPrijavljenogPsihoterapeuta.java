b855776addfeccb3468e8c1b4ed253f06652861b
Ilija <04ilijapavlovic@gmail.com>
2025-05-04 13:16:44 +0200

    forme

--- OOP-JavaFX/src/main/java/app/View/PPrijavaNovihKlijenataKodPrijavljenogPsihoterapeuta.java
+++ OOP-JavaFX/src/main/java/app/View/PPrijavaNovihKlijenataKodPrijavljenogPsihoterapeuta.java
@@ -1,4 +1,84 @@
 package app.View;
 
-public class PPrijavaNovihKlijenataKodPrijavljenogPsihoterapeuta {
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class PPrijavaNovihKlijenataKodPrijavljenogPsihoterapeuta extends HBox {
+    //Mozda nije lepo napravljena foram treba pregledati opet tekst specifikacije
+    private ComboBox<String> cbPsihoterapeuti;
+    private Label lbNaslov;
+    private Label lbIzborPsihoterapeuta;
+    private Button btnIzaberi;
+    private ObservableList<String> olNoviKlijenti;
+    private TableView twNoviKlijenti;
+    private VBox vb;
+    private VBox vb2;
+    private HBox hb;
+
+    public PPrijavaNovihKlijenataKodPrijavljenogPsihoterapeuta(){
+        napraviElemente();
+        dodajElemente();
+        dodajAkcije();
+    }
+
+    private void napraviElemente()
+    {
+        lbIzborPsihoterapeuta = new Label("Izaberite psihoterapeuta");
+        lbNaslov = new Label("Pregled prijava novih klijenata kod prijavljenog psihoterapeuta");
+        btnIzaberi = new Button("Izaberi");
+        twNoviKlijenti.setMinWidth(400);
+        twNoviKlijenti = new TableView<>(olNoviKlijenti);
+        vb = new VBox();
+        vb.setPadding(new Insets(15,10,15,10));
+        vb.setAlignment(Pos.CENTER);
+        vb.setSpacing(10);
+        hb = new HBox();
+        hb.setAlignment(Pos.CENTER);
+        hb.setPadding(new Insets(15,10,15,10));
+        hb.setSpacing(10);
+        vb2 = new VBox();
+        vb2.setPadding(new Insets(15,10,15,10));
+        vb2.setAlignment(Pos.CENTER);
+        vb2.setSpacing(10);
+        cbPsihoterapeuti = new ComboBox<>();
+        //TODO dodati iteme u comboBox i zavrsiti tabelu
+    /*
+        TableColumn<**, String> col1 = new TableColumn<>("nazivkolone");
+        TableColumn<** , String> col2 = new TableColumn<>("nazivkolone");
+        TableColumn<**, String> col3 = new TableColumn<>("nazivkolone");
+
+        col1.setCellValueFactory(new PropertyValueFactory<>("id"));
+        col2.setCellValueFactory(new PropertyValueFactory<>("kontinent"));
+        col3.setCellValueFactory(new PropertyValueFactory<>("drzava"));
+
+        twNoviKlijenti.getColumns().addAll(col1, col2, col3);
+
+     */
+    }
+
+    private void dodajElemente()
+    {
+        vb2.getChildren().addAll(lbIzborPsihoterapeuta, cbPsihoterapeuti);
+        hb.getChildren().addAll(vb2, btnIzaberi);
+        vb.getChildren().addAll(hb, twNoviKlijenti);
+        this.setAlignment(Pos.CENTER);
+        this.setPadding(new Insets(15,10,15,10));
+        this.setSpacing(15);
+        this.getChildren().addAll(lbNaslov, vb);
+    }
+
+    private void dodajAkcije()
+    {
+
+    }
+
+
+
 }
--- OOP-JavaFX/src/main/java/app/View/PregledUplataIDugovanjaKlijenata.java
+++ OOP-JavaFX/src/main/java/app/View/PregledUplataIDugovanjaKlijenata.java
@@ -1,4 +1,55 @@
 package app.View;
 
-public class PregledUplataIDugovanjaKlijenata {
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+
+import javafx.scene.control.Label;
+
+public class PregledUplataIDugovanjaKlijenata extends HBox {
+    private Label lbNaslov;
+    private ObservableList<String> olUplateDugovi;
+    private TableView twUplatedDugovi;
+
+    public PregledUplataIDugovanjaKlijenata(){
+        napraviElemente();
+        dodajElemente();
+        dodajAkcije();
+    }
+    private void napraviElemente()
+    {
+        lbNaslov = new Label("Pregled uplata i dugovanja klijenata");
+        twUplatedDugovi.setMinWidth(500);
+        twUplatedDugovi = new TableView<>(olUplateDugovi);
+
+         /*
+        TableColumn<**, String> col1 = new TableColumn<>("nazivkolone");
+        TableColumn<** , String> col2 = new TableColumn<>("nazivkolone");
+        TableColumn<**, String> col3 = new TableColumn<>("nazivkolone");
+
+        col1.setCellValueFactory(new PropertyValueFactory<>("id"));
+        col2.setCellValueFactory(new PropertyValueFactory<>("kontinent"));
+        col3.setCellValueFactory(new PropertyValueFactory<>("drzava"));
+
+        twNoviKlijenti.getColumns().addAll(col1, col2, col3);
+
+     */
+    }
+
+    private void dodajElemente()
+    {
+        this.setAlignment(Pos.CENTER);
+        this.setPadding(new Insets(15,10,15,10));
+        this.setSpacing(30);
+        this.getChildren().addAll(lbNaslov, twUplatedDugovi);
+
+    }
+
+    private void dodajAkcije()
+    {
+
+    }
+
 }
