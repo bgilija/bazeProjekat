54532632ab5e2ff63d6b08774a33e8af89eea1ee
Ilija <04ilijapavlovic@gmail.com>
2025-05-04 14:01:33 +0200

    forme

--- OOP-JavaFX/src/main/java/app/View/PregledBeleskiIOdredjenihTestova.java
+++ OOP-JavaFX/src/main/java/app/View/PregledBeleskiIOdredjenihTestova.java
@@ -1,4 +1,58 @@
 package app.View;
 
-public class PregledBeleskiIOdredjenihTestova {
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+//todo u er dijagram dodati polje beleska ili novi entitet
+public class PregledBeleskiIOdredjenihTestova extends VBox {
+
+    private Label lblNaslov;
+    private Label lblIdPsihoterapeuta;
+    private Label lblImePrezimePsihoterapeuta;
+    private Label lblIdKandidata;
+    private Label lblImePrezimeKandidata;
+    private Label lblBrojSeanse;
+    private TextArea txtBeleska;
+    private Button btnIzmeniBelesku;
+
+    public PregledBeleskiIOdredjenihTestova(
+            int idPsihoterapeuta,
+            String imePsihoterapeuta,
+            String prezimePsihoterapeuta,
+            int idKandidata,
+            String imeKandidata,
+            String prezimeKandidata,
+            int brojSeanse,
+            String beleska
+    ) {
+        lblNaslov = new Label("Pregled beleški sa testiranja");
+        lblIdPsihoterapeuta = new Label("ID psihoterapeuta: " + idPsihoterapeuta);
+        lblImePrezimePsihoterapeuta = new Label("Psihoterapeut: " + imePsihoterapeuta + " " + prezimePsihoterapeuta);
+        lblIdKandidata = new Label("ID kandidata: " + idKandidata);
+        lblImePrezimeKandidata = new Label("Kandidat: " + imeKandidata + " " + prezimeKandidata);
+        lblBrojSeanse = new Label("Broj seanse: " + brojSeanse);
+
+        txtBeleska = new TextArea(beleska);
+        txtBeleska.setWrapText(true);
+        txtBeleska.setEditable(false);
+        txtBeleska.setPrefHeight(200);
+
+        btnIzmeniBelesku = new Button("Izmeni belešku");
+//        btnIzmeniBelesku.setOnAction(e -> {
+//            boolean trenutnoStanje = txtBeleska.isEditable();
+//            txtBeleska.setEditable(!trenutnoStanje);
+//            if (txtBeleska.isEditable()) {
+//                btnIzmeniBelesku.setText("Završi izmenu");
+//            } else {
+//                btnIzmeniBelesku.setText("Izmeni belešku");
+//                // Ovdje možeš dodati logiku za čuvanje izmenjene beleške u bazu
+//            }
+//        });
+
+        this.setSpacing(10);
+        this.getChildren().addAll(lblNaslov,lblIdPsihoterapeuta,lblImePrezimePsihoterapeuta,lblIdKandidata,lblImePrezimeKandidata,lblBrojSeanse,
+                new Label("Beleška:"),txtBeleska,btnIzmeniBelesku
+        );
+    }
 }
--- OOP-JavaFX/src/main/java/app/View/PregledProfilaPsihoterapeuta.java
+++ OOP-JavaFX/src/main/java/app/View/PregledProfilaPsihoterapeuta.java
@@ -1,4 +1,54 @@
 package app.View;
 
-public class PregledProfilaPsihoterapeuta {
+import javafx.scene.layout.VBox;
+
+import javafx.scene.control.Label;
+
+//ime, prezime, id, brojTelefona,fakultet, uze usmerenje, bool psiholog,datum rodjenja, prebivaliste.
+public class PregledProfilaPsihoterapeuta extends VBox {
+    private Label lblNaslov;
+    private Label lblIme;
+    private Label lblPrezime;
+    private Label lblBrTelefona;
+    private Label lblFakultet;
+    private Label lblUzeUsmerenje;
+    private Label lblDatumRodjenja;
+    private Label lblPrebivaliste;
+    private Label lblPsiholog;
+    public PregledProfilaPsihoterapeuta(
+            String ime,
+            String prezime,
+            String brojTelefona,
+            String fakultet,
+            String uzeUsmerenje,
+            boolean psiholog,
+            String datumRodjenja,
+            String prebivaliste
+    ) {
+        lblNaslov = new Label("Profil psihoterapeuta");
+        lblIme = new Label("Ime: " + ime);
+        lblPrezime = new Label("Prezime: " + prezime);
+        lblBrTelefona = new Label("Broj telefona: " + brojTelefona);
+        lblFakultet = new Label("Fakultet: " + fakultet);
+        lblUzeUsmerenje = new Label("Uže usmerenje: " + uzeUsmerenje);
+        lblPsiholog = new Label("Psiholog: " + (psiholog ? "Da" : "Ne"));
+        lblDatumRodjenja = new Label("Datum rođenja: " + datumRodjenja);
+        lblPrebivaliste = new Label("Prebivalište: " + prebivaliste);
+
+
+        this.setSpacing(10);
+
+
+        this.getChildren().addAll(
+                lblNaslov,
+                lblIme,
+                lblPrezime,
+                lblBrTelefona,
+                lblFakultet,
+                lblUzeUsmerenje,
+                lblPsiholog,
+                lblDatumRodjenja,
+                lblPrebivaliste
+        );
+    }
 }
